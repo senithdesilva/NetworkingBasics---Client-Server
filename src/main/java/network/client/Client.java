fa31c7775d829a2cd019ef9803766c84fca47eb7
Senith De Silva <senith.desilva@yahoo.co.uk>
2019-01-15 15:30:12 +1100

    Update Client Connection

--- src/main/java/network/client/Client.java
+++ src/main/java/network/client/Client.java
@@ -12,19 +12,23 @@ public class Client {
     private static final Integer PORT = 7777;
 
     public static void main(String[] args) {
-
+        // The client instantiates a Socket object, specifying the server name and the port number to connect to.
+        // In this case, server is local host and the server port is 7777 as specified.
         try (Socket client = new Socket(SERVER_NAME, PORT)) {
 
             LOGGER.info("Connected to Server: " + client.getRemoteSocketAddress());
+
+            // The client's OutputStream is connected to the server's InputStream
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
-
             out.writeUTF("Hello from " + client.getLocalSocketAddress());
-            InputStream inFromServer = client.getInputStream();
-            DataInputStream in = new DataInputStream(inFromServer);
+
+            // The client's InputStream is connected to the server's OutputStream.
+            InputStream inputStreamFromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(inputStreamFromServer);
             String serverResponse = in.readUTF();
 
-            LOGGER.config("Server Response: " + serverResponse);
+            LOGGER.info("Server Response: " + serverResponse);
 
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "Exception Occurred: ", e);
